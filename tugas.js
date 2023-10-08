4a231d3aa188ade62ee236cda2429a7758382d19
hamputra12 <newilham30^@gmail.com>
2023-10-08 08:02:59 +0700

    first commit

---
+++ tugas.js
@@ -0,0 +1,85 @@
+// if, else
+// pacar = prompt('Berapa pacar kamu?')
+
+// if (pacar == 0) {
+//     alert ('Kasihan jomblo, cari sono gih!')
+// }   else if (pacar == 1) {
+//     alert ('Yakin g mau nambah? 2 bolehlah')
+// }   else {
+//     alert ('Cukup duit lu buat ngebayarin mereka?')
+// }
+
+
+// nested if
+// username = prompt ('Username:')
+// pasword = prompt ('Password:')
+
+// if (username == 'IlhamPR') {
+//     if (pasword == '12345678') {
+//         alert ('Selamat Datang IlhamPR')
+//     }   else {
+//         alert ('Username atau Password salah, Coba lagi')
+//     }
+// }   else {
+//     alert ('Anda tidak terdaftar')
+// }
+
+
+// switch case
+// mood = prompt ('Rate 1 - 5. Gimana mood-mu hari ini?')
+
+// switch (mood) {
+//     case '1':
+//         alert('abis diputusin pacar ya?')
+//         break
+//     case '2':
+//         alert('lagi sedih?')
+//         break
+//     case '3':
+//         alert('butuh motivasi?')
+//         break
+//     case '4':
+//         alert('kurang ngopi pagi kali')
+//         break
+//     case '5':
+//         alert('pasti abis diterima crush lo')
+//         break
+//     default:
+//         alert('Semoga harimu menyenagkan')
+//         break
+// }
+
+
+// for statement
+// const nameCars = ['toyota', 'avanza', 'kijang', 'brio']
+// for (let car = 0; car < nameCars.length; car++) {
+//     console.log(nameCars[car]);
+// }
+
+
+// while loop
+// let pasien = 1
+// while (pasien <= 10) {
+//     console.log('Antrean ' + pasien)
+//     pasien++
+// }
+
+
+// do while 
+// let i = 1
+// do {
+//     console.log(i)
+//     i++
+// }   while (i <= 5)
+
+
+// function
+// function angka (a, b) {
+//     console.log(a);
+//     console.log(a + b);
+//     console.log(a*b);
+//     console.log(a/b);
+
+// }
+
+// angka (4, 2);
